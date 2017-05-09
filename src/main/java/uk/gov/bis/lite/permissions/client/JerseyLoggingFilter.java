119d164d0efe1eab4e3d4e276ae03f1d2d68d26a
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-05-09 11:24:08 +0100

    Renamed LoggingFilter to JerseyLoggingFilter, correlation id filter priority set above the logging filter

--- src/main/java/uk/gov/bis/lite/permissions/client/JerseyLoggingFilter.java
+++ src/main/java/uk/gov/bis/lite/permissions/client/JerseyLoggingFilter.java
@@ -10,21 +10,21 @@ import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.client.ClientResponseFilter;
 
-public class LoggingFilter implements ClientRequestFilter, ClientResponseFilter {
-  private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
+public class JerseyLoggingFilter implements ClientRequestFilter, ClientResponseFilter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(JerseyLoggingFilter.class);
 
-  public LoggingFilter() {}
+  public JerseyLoggingFilter() {}
 
   public void filter(ClientRequestContext requestContext) throws IOException {
     String method = requestContext.getMethod();
     String uri = requestContext.getUri().toString();
-    LOGGER.info("Request {} {}", method, uri);
+    LOGGER.info("JerseyRequest {} {}", method, uri);
   }
 
   public void filter(ClientRequestContext requestContext, ClientResponseContext responseContext) throws IOException {
     int statusCode = responseContext.getStatus();
     String statusReason = responseContext.getStatusInfo().getReasonPhrase();
-    LOGGER.info("Response {} {}", statusCode, statusReason);
+    LOGGER.info("JerseyResponse {} {}", statusCode, statusReason);
   }
 }
 
