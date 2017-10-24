547e6943d6241282f27f4dce54272f2b92f2e015
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-10-24 16:32:15 +0100

    Configured auth filters

--- src/main/java/uk/gov/bis/lite/permissions/PermissionsApp.java
+++ src/main/java/uk/gov/bis/lite/permissions/PermissionsApp.java
@@ -1,16 +1,19 @@
 package uk.gov.bis.lite.permissions;
 
 import com.github.toastshaman.dropwizard.auth.jwt.JwtAuthFilter;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.Module;
 import io.dropwizard.Application;
-import io.dropwizard.auth.AuthDynamicFeature;
-import io.dropwizard.auth.AuthValueFactoryProvider;
+import io.dropwizard.auth.PolymorphicAuthDynamicFeature;
+import io.dropwizard.auth.PolymorphicAuthValueFactoryProvider;
 import io.dropwizard.auth.PrincipalImpl;
 import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.flywaydb.core.Flyway;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
 import ru.vyarus.dropwizard.guice.module.installer.feature.ManagedInstaller;
 import ru.vyarus.dropwizard.guice.module.installer.feature.jersey.ResourceInstaller;
@@ -59,17 +62,23 @@ public class PermissionsApp extends Application<PermissionsAppConfig> {
 
     String jwtSharedSecret = config.getJwtSharedSecret();
 
-    JwtAuthFilter<LiteJwtUser> liteJwtUserJwtAuthFilter = LiteJwtAuthFilterHelper.buildAuthFilter(jwtSharedSecret);
-
-    environment.jersey().register(new AuthDynamicFeature(liteJwtUserJwtAuthFilter));
-
-    environment.jersey().register(new AuthValueFactoryProvider.Binder<>(LiteJwtUser.class));
-
-    environment.jersey().register(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<PrincipalImpl>()
+    BasicCredentialAuthFilter<PrincipalImpl> basicAuthFilter = new BasicCredentialAuthFilter.Builder<PrincipalImpl>()
         .setAuthenticator(new SimpleAuthenticator(config.getAdminLogin(), config.getAdminPassword()))
         .setRealm("Permissions Service Admin Authentication")
-        .buildAuthFilter()));
+        .buildAuthFilter();
 
+    JwtAuthFilter<LiteJwtUser> liteJwtUserJwtAuthFilter = LiteJwtAuthFilterHelper.buildAuthFilter(jwtSharedSecret);
+
+    PolymorphicAuthDynamicFeature authFeature = new PolymorphicAuthDynamicFeature(
+        ImmutableMap.of(
+            PrincipalImpl.class, basicAuthFilter,
+            LiteJwtUser.class, liteJwtUserJwtAuthFilter));
+
+    AbstractBinder authBinder = new PolymorphicAuthValueFactoryProvider.Binder<>(
+        ImmutableSet.of(PrincipalImpl.class, LiteJwtUser.class));
+
+    environment.jersey().register(authFeature);
+    environment.jersey().register(authBinder);
     environment.jersey().register(ContainerCorrelationIdFilter.class);
 
     // Perform/validate flyway migration on startup
