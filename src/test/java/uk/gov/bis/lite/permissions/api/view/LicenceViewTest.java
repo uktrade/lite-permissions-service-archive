14a420be59f177bd39987a5f5bac9f92135b906c
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-10-31 17:09:50 +0000

    ObjectMapper initialization moved to @Before

--- src/test/java/uk/gov/bis/lite/permissions/api/view/LicenceViewTest.java
+++ src/test/java/uk/gov/bis/lite/permissions/api/view/LicenceViewTest.java
@@ -11,30 +11,31 @@ import static uk.gov.bis.lite.permissions.api.view.LicenceTestUtil.generateLicen
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
 
 public class LicenceViewTest {
 
-  @Test
-  public void localDateSerialization() throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
+  private ObjectMapper mapper;
+
+  @Before
+  public void setUp() throws Exception {
+    mapper = new ObjectMapper();
     mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     mapper.registerModule(new JavaTimeModule());
+  }
 
+  @Test
+  public void localDateSerialization() throws Exception {
     String json = mapper.writeValueAsString(LocalDate.of(2000,12,31)); // "2000-12-31"
-    System.out.println(json);
     LocalDate localDate = mapper.readValue(json, LocalDate.class);
     assertThat(localDate).isEqualTo("2000-12-31");
   }
 
   @Test
   public void testLicenceViewA() throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    mapper.registerModule(new JavaTimeModule());
-
     String json = mapper.writeValueAsString(generateLicenceViewA());
     LicenceView licenceView = mapper.readValue(json, LicenceView.class);
     assertLicenceViewA(licenceView);
@@ -42,10 +43,6 @@ public class LicenceViewTest {
 
   @Test
   public void testLicenceViewB() throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    mapper.registerModule(new JavaTimeModule());
-
     String json = mapper.writeValueAsString(generateLicenceViewB());
     LicenceView licenceView = mapper.readValue(json, LicenceView.class);
     assertLicenceViewB(licenceView);
@@ -53,10 +50,6 @@ public class LicenceViewTest {
 
   @Test
   public void testLicenceViewC() throws Exception {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    mapper.registerModule(new JavaTimeModule());
-
     String json = mapper.writeValueAsString(generateLicenceViewC());
     LicenceView licenceView = mapper.readValue(json, LicenceView.class);
     assertLicenceViewC(licenceView);
